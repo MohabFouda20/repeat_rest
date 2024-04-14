7eee2be9dc79e9932af584e72e4672b6458c6b37
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-04-14 13:09:56 +0200

    i dont know why the main urls have error

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from . models import Product
+
+
+
+from rest_framework import generics
+from . serializers import productSerializer
 
 # Create your views here.
+class productDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = productSerializer
+    
+product_view = productDetailAPIView.as_view() # this is a class based view
