7095e76776c12a3f2bb7b43e953a8038a7868c04
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2024-04-17 01:46:49 +0200

    now the viewsets and routers are much summarize than views and urls need to search to know more about the diffrnce

---
+++ backend/cfehome/routers.py
@@ -0,0 +1,9 @@
+from rest_framework.routers import DefaultRouter
+from products.viewsets import ProductViewSet
+
+
+router = DefaultRouter()
+router.register('products-111', ProductViewSet , basename= 'products')
+
+print(router.urls)
+urlpatterns = router.urls   # This is a list of urls that we can include in our urls.py file
